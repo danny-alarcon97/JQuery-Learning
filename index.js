74f57c5413a7f76c4098edb7152be2f7f0325cca
daniel <danny.alarcon1997@gmail.com>
2022-10-18 13:05:08 -0400

    adding/removing classes

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-$("h1").css("color", "red");
+// same as document.querySelector("h1")
+// $("h1").css("color", "red");
+
+// Manipulate styles & adding multiple classes
+// $("h1").addClass("big-title");
+// $("h1").removeClass("big-title");
+
+// $("h1").addClass("big-title margin-50");
